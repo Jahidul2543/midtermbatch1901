4e849cc7ce88cee25b82a547c9b87ee06524b733
Md Jahidul Islam <jahidul@Mds-MacBook-Pro.fios-router.home>
2019-05-11 07:02:11 -0400

    Mid Term-Batch 1901

---
+++ src/main/java/api/DeepDiveArrayList.java
@@ -0,0 +1,12 @@
+package api;
+
+public class DeepDiveArrayList {
+
+    /**
+     * @author Jahiudl Islam
+     * Read ArrayList JDK 8 API documantation to be read and ArrayList source code to be viewed
+     * Demonastrate your understanding on ArrayList after reading the document
+     * You are free to document your understanding using comment
+     *
+     * */
+}
---
+++ src/main/java/java/io/DataReader.java
@@ -0,0 +1,17 @@
+package java.io;
+
+public class DataReader {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Use API to read the below textFile and print to console.
+		 * /src/main/resources/Key-To-Success
+		 * Use BufferedReader class. 
+		 * Use try....catch block to handle Exception.
+		 * Use ForEach loop/while loop/Iterator to retrieve data.
+		 */
+
+	}
+
+}
